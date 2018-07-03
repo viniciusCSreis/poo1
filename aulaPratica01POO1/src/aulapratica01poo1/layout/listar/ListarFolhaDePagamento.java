3cfd235c50e1fde37aaaed2512c1f6607c5655a5
Vinicius Clemente <viniciuscsreis@gmail.com>
2018-07-02 22:35:48 -0300

    1.0.1
    Trabalho enviado com ultima correção de bug

--- aulaPratica01POO1/src/aulapratica01poo1/layout/listar/ListarFolhaDePagamento.java
+++ aulaPratica01POO1/src/aulapratica01poo1/layout/listar/ListarFolhaDePagamento.java
@@ -53,7 +53,7 @@ public class ListarFolhaDePagamento extends javax.swing.JFrame {
         sLiquido = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel2.setText("Selecione um Funcionário:");
 
